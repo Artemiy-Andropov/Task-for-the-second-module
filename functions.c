bab5feab05c3557703e1cb32bf8d6d8b487ae04d
Artemiy-Andropov <91526153+Artemiy-Andropov@users.noreply.github.com>
2021-12-23 21:31:20 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <locale.h>
 
 // Я так и не смог до конца исправить это задание
@@ -8,49 +9,52 @@ void Exercise_3();
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    Exercise_3();
-    return 0;
+   setlocale(LC_ALL, "Russian");
+   Exercise_3();
+   return 0;
 }
 
 void Exercise_3()
 {
-    int counter = 0;
-    
-    char str[200];
-    
-    char str2[200];
-    
-    FILE *Russian_text;
-    
-    Russian_text = fopen("f.txt", "r");
-    
-    FILE *No_P4;
-    
-    No_P4 = fopen("g.txt", "w");
+   int counter = 0;
 
-    fgets(str, 200, Russian_text);
-    
-    for(int i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ' && counter != 3 && str[i] != 'П' && str[i] != 'п')
-        {
-            str2[i] = str[i];
-            printf("%s", str2);
-            counter++;
-        }
-        if (counter == 3 && str[i] == ' ')
-        {
-            counter = 0;
-        }
-        if (str[i] == 'П' && str[i] == 'п')
-        {
-            counter++;
-        }
-    }
-    
-    fprintf(No_P4, "%s", str2);
+   char str[200];
 
-    fclose(Russian_text);
-    fclose(No_P4);
+   char str2[200];
+
+   FILE* Russian_text;
+
+   fopen_s(&Russian_text, "f.txt", "r");
+
+   FILE* No_P4;
+
+   fopen_s(&No_P4, "g.txt", "w");
+
+   if (Russian_text != NULL)
+      fgets(str, 200, Russian_text);
+
+   for (int i = 0; str[i] != '\0'; i++)
+   {
+      if (counter != 3)
+         str2[i] = str[i];
+      if (str[i] == ' ' && counter != 3)
+      {
+         counter++;
+      }
+      if (counter == 3 && str[i] == ' ')
+      {
+         counter = 0;
+      }
+      if (i > 1)
+      {
+         if ((str[i] == 'П' || str[i] == 'п') && str[i - 1] == ' ')
+            counter++;
+      }
+   }
+   if (No_P4 != NULL && Russian_text != NULL)
+   {
+      fprintf(No_P4, "%s", str2);
+      fclose(Russian_text);
+      fclose(No_P4);
+   }
 }
